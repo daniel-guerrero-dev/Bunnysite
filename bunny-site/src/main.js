464aaad15ff2381c388352610b2b8a48699aee59
daniel-guerrero-dev <danielguerrerodev6@gmail.com>
2026-07-14 16:21:09 -0500

    Added functionality that allows finding bunny images

--- bunny-site/src/main.js
+++ bunny-site/src/main.js
@@ -1,1 +1,24 @@
 import "./style.css";
+
+async function newRabbit() {
+  const url =
+    "https://corsproxy.io/?url=https://rabbit-api-two.vercel.app/api/random";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+let bunny = await newRabbit();
+
+const bunnyImg = document.querySelector('[data-image="bunny-image"]');
+
+bunnyImg.src = bunny.url;
